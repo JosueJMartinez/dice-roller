d546919e4b144f010b9722af6fdcd48658a652ab
JosueJMartinez <jmarti61@gmail.com>
2020-09-04 12:22:55 -0500

    Updated aesthitics and matching if you win

--- src/components/RollDice.jsx
+++ src/components/RollDice.jsx
@@ -3,7 +3,7 @@ import Die from './Die';
 import '../css/RollDice.css';
 
 export default class RollDice extends Component {
-	state = { die1: 'one', die2: 'one', isClicked: false };
+	state = { die1: 'one', die2: 'one', isClicked: false, isMatched: false };
 
 	static defaultProps = {
 		nums: [ 'one', 'two', 'three', 'four', 'five', 'six' ]
@@ -11,11 +11,21 @@ export default class RollDice extends Component {
 	handleClick = e => {
 		const die1 = Math.floor(Math.random() * 6);
 		const die2 = Math.floor(Math.random() * 6);
+
 		this.setState({
-			isClicked: true
+			isClicked: true,
+			isMatched: false
 		});
 		setTimeout(() => {
-			this.setState({
+			if (die1 === die2) {
+				return this.setState({
+					die1: this.props.nums[die1],
+					die2: this.props.nums[die2],
+					isClicked: false,
+					isMatched: true
+				});
+			}
+			return this.setState({
 				die1: this.props.nums[die1],
 				die2: this.props.nums[die2],
 				isClicked: false
@@ -36,6 +46,7 @@ export default class RollDice extends Component {
 				>
 					{this.state.isClicked ? 'Rolling...' : 'Click to Roll'}
 				</button>
+				{this.state.isMatched && <h1>Congrats there is a match</h1>}
 			</div>
 		);
 	}
